2116ac2e46528dbe33055b6a42673de6740dc857
Andy Pickler <andy.pickler@gmail.com>
2022-09-15 10:33:26 -0600

    Step 3

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const { ApolloServer, gql, AuthenticationError } = require('apollo-server');
-const { readFileSync } = require('fs');
-const axios = require('axios');
+const { ApolloServer, gql, AuthenticationError } = require("apollo-server");
+const { readFileSync } = require("fs");
+const axios = require("axios");
 
-const typeDefs = gql(readFileSync('./schema.graphql', { encoding: 'utf-8' }));
-const resolvers = require('./resolvers');
-const { BookingsDataSource, ReviewsDataSource, ListingsAPI, AccountsAPI, PaymentsAPI } = require('./services');
+const typeDefs = gql(readFileSync("./schema.graphql", { encoding: "utf-8" }));
+const resolvers = require("./resolvers");
+const {
+  BookingsDataSource,
+  ReviewsDataSource,
+  ListingsAPI,
+  AccountsAPI,
+  PaymentsAPI,
+} = require("./services");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const server = new ApolloServer({
   typeDefs,
@@ -21,8 +27,8 @@ const server = new ApolloServer({
     };
   },
   context: async ({ req }) => {
-    const token = req.headers.authorization || '';
-    const userId = token.split(' ')[1]; // get the user name after 'Bearer '
+    const token = req.headers.authorization || "";
+    const userId = token.split(" ")[1]; // get the user name after 'Bearer '
     if (userId) {
       const { data } = await axios
         .get(`http://localhost:4011/login/${userId}`)
